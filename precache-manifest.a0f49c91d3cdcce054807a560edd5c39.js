d41af475bf0e7ea68d08e2b993a71295040e638b
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2020-10-18 02:53:46 -0500

    Updates

---
+++ precache-manifest.a0f49c91d3cdcce054807a560edd5c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5003ffada9bdd027f88156972ae1a14a",
+    "url": "/rolodex-monster-browser/index.html"
+  },
+  {
+    "revision": "213a0203769b71f78b74",
+    "url": "/rolodex-monster-browser/static/css/main.76467fdb.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/rolodex-monster-browser/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rolodex-monster-browser/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213a0203769b71f78b74",
+    "url": "/rolodex-monster-browser/static/js/main.01549f34.chunk.js"
+  },
+  {
+    "revision": "5f4fe62af555e4e449be",
+    "url": "/rolodex-monster-browser/static/js/runtime-main.5df83a6b.js"
+  }
+]);
